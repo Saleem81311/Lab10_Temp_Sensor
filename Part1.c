8969e645692997a109626109160b203da0da6da5
10themann <93688045+10themann@users.noreply.github.com>
2021-11-04 19:34:15 -0400

    Update Part1.c

--- Part1.c
+++ Part1.c
@@ -1,7 +1,3 @@
-#include "msp.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /*****************************************************************************************************************
  * Editors: Saleem Griggs-Taylor, Kevin Figurski, Tennison Hoffmann
  * Start Date: 11/4/2021
@@ -9,6 +5,10 @@
  * Description: This program is developed to interface a temperature sensor with the MSP432 while also
  * displaying the values to a LCD and using a potentiometer to control output of the temperature sensor.
  *****************************************************************************************************************/
+
+#include "msp.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void main(void)
 {
