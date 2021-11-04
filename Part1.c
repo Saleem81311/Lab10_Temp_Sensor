d336ed93e57bd1ca4171fd69251b0290f7dddade
Saleem81311 <93357293+Saleem81311@users.noreply.github.com>
2021-11-04 19:32:43 -0400

    Update Part1.c

--- Part1.c
+++ Part1.c
@@ -1,1 +1,16 @@
+#include "msp.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/*****************************************************************************************************************
+ * Editors: Saleem Griggs-Taylor, Kevin Figurski, Tennison Hoffmann
+ * Start Date: 11/4/2021
+ * Last modified: 11/4/2021
+ * Description: This program is developed to interface a temperature sensor with the MSP432 while also
+ * displaying the values to a LCD and using a potentiometer to control output of the temperature sensor.
+ *****************************************************************************************************************/
+
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+}
